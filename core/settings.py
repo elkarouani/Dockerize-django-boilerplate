c3ec1e4f0e8dafe291ba72f1e9aa40e269260b83
elkarouani <elkarouani@gmail.com>
2021-01-28 16:19:51 +0000

    Fix: again static foulder not found

--- core/settings.py
+++ core/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app/build/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
